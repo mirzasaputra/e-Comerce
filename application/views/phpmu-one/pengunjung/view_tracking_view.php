f1d120ca9a8714fd54fccfcd3bde4a6b1019e113
dafidpr <dafidpr@gmail.com>
2020-09-05 21:06:00 +0700

    Perbaikan penulisan model_app jadi Model_app

--- application/views/phpmu-one/pengunjung/view_tracking_view.php
+++ application/views/phpmu-one/pengunjung/view_tracking_view.php
@@ -1,7 +1,15 @@
 <p class='sidebar-title'><?php echo $title; ?></p>
-<?php 
-if ($total['proses']=='0'){ $proses = '<i class="text-danger">Pending</i>'; }elseif($total['proses']=='1'){ $proses = '<i class="text-warning">Proses</i>'; }elseif($total['proses']=='2'){ $proses = '<i class="text-info">Konfirmasi</i>'; }else{ $proses = '<i class="text-success">Packing </i>'; }
-  echo "<div class='col-md-7'>
+<?php
+if ($total['proses'] == '0') {
+  $proses = '<i class="text-danger">Pending</i>';
+} elseif ($total['proses'] == '1') {
+  $proses = '<i class="text-warning">Proses</i>';
+} elseif ($total['proses'] == '2') {
+  $proses = '<i class="text-info">Konfirmasi</i>';
+} else {
+  $proses = '<i class="text-success">Packing </i>';
+}
+echo "<div class='col-md-7'>
         <dl class='dl-horizontal'>
             <dt>Nama</dt>       <dd>$rows[nama_lengkap]</dd>
             <dt>No Telpon/Hp</dt>       <dd>$rows[no_hp]</dd>
@@ -14,13 +22,13 @@ if ($total['proses']=='0'){ $proses = '<i class="text-danger">Pending</i>'; }els
     <div class='col-md-5'>
         <center>
         Total Tagihan 
-        <h4 style='margin:0px;'>Rp ".rupiah($total['total']+$total['ongkir']+substr($kode_transaksi,-3))."<br> <br> ";
-        if ($total['resi']!=''){
-          echo "<span style='color:red; font-size:14px'>No Resi : <b>$total[resi]</b></span><br>";
-        }else{
-          echo "<span style='color:red; font-size:14px'>No Resi : <b>-</b></span><br>";
-        }
-        echo "<span style='text-transform:uppercase'>$total[kurir]</span> ($total[service])
+        <h4 style='margin:0px;'>Rp " . rupiah($total['total'] + $total['ongkir'] + substr($kode_transaksi, -3)) . "<br> <br> ";
+if ($total['resi'] != '') {
+  echo "<span style='color:red; font-size:14px'>No Resi : <b>$total[resi]</b></span><br>";
+} else {
+  echo "<span style='color:red; font-size:14px'>No Resi : <b>-</b></span><br>";
+}
+echo "<span style='text-transform:uppercase'>$total[kurir]</span> ($total[service])
         </h4>
         Status : <i>$proses</i>   
         </center>
@@ -38,43 +46,51 @@ if ($total['proses']=='0'){ $proses = '<i class="text-danger">Pending</i>'; }els
           </thead>
           <tbody>";
 
-          $no = 1;
-          $diskon_total = 0;
-          foreach ($record->result_array() as $row){
-          $sub_total = (($row['harga_jual']-$row['diskon'])*$row['jumlah']);
-          if ($row['diskon']!='0'){ $diskon = "<del style='color:red'>".rupiah($row['harga_jual'])."</del>"; }else{ $diskon = ""; }
-          if (trim($row['gambar'])==''){ $foto_produk = 'no-image.png'; }else{ $foto_produk = $row['gambar']; }
-          $diskon_total = $diskon_total+$row['diskon']*$row['jumlah'];
-          echo "<tr>
-                    <td class='valign'><a href='".base_url()."produk/detail/$row[produk_seo]'>$row[nama_produk]</a><br>
+$no = 1;
+$diskon_total = 0;
+foreach ($record->result_array() as $row) {
+  $sub_total = (($row['harga_jual'] - $row['diskon']) * $row['jumlah']);
+  if ($row['diskon'] != '0') {
+    $diskon = "<del style='color:red'>" . rupiah($row['harga_jual']) . "</del>";
+  } else {
+    $diskon = "";
+  }
+  if (trim($row['gambar']) == '') {
+    $foto_produk = 'no-image.png';
+  } else {
+    $foto_produk = $row['gambar'];
+  }
+  $diskon_total = $diskon_total + $row['diskon'] * $row['jumlah'];
+  echo "<tr>
+                    <td class='valign'><a href='" . base_url() . "produk/detail/$row[produk_seo]'>$row[nama_produk]</a><br>
                     <small>Note : $row[keterangan_order]</small></td>
-                    <td class='valign'>".rupiah($row['harga_jual']-$row['diskon'])." $diskon</td>
+                    <td class='valign'>" . rupiah($row['harga_jual'] - $row['diskon']) . " $diskon</td>
                     <td class='valign'>$row[jumlah]</td>
-                    <td class='valign'>".($row['berat']*$row['jumlah'])." Gram</td>
-                    <td class='valign'>Rp ".rupiah($sub_total)."</td>
+                    <td class='valign'>" . ($row['berat'] * $row['jumlah']) . " Gram</td>
+                    <td class='valign'>Rp " . rupiah($sub_total) . "</td>
                 </tr>";
-            $no++;
-          }
-          
-          echo "<tr class='success'>
-                  <td colspan='4'><b>Subtotal </b> <i class='pull-right'>(".terbilang($total['total'])." Rupiah)</i></td>
-                  <td><b>Rp ".rupiah($total['total'])."</b></td>
+  $no++;
+}
+
+echo "<tr class='success'>
+                  <td colspan='4'><b>Subtotal </b> <i class='pull-right'>(" . terbilang($total['total']) . " Rupiah)</i></td>
+                  <td><b>Rp " . rupiah($total['total']) . "</b></td>
                 </tr>
 
                 <tr class='success'>
-                  <td colspan='4'><b>Ongkir </b> <i class='pull-right'>(".terbilang($total['ongkir'])." Rupiah)</i></td>
-                  <td><b>Rp ".rupiah($total['ongkir'])."</b></td>
+                  <td colspan='4'><b>Ongkir </b> <i class='pull-right'>(" . terbilang($total['ongkir']) . " Rupiah)</i></td>
+                  <td><b>Rp " . rupiah($total['ongkir']) . "</b></td>
                 </tr>
 
                 <tr class='success'>
-                  <td colspan='4'><b>Berat</b> <i class='pull-right'>(".terbilang($total['total_berat'])." Gram)</i></td>
+                  <td colspan='4'><b>Berat</b> <i class='pull-right'>(" . terbilang($total['total_berat']) . " Gram)</i></td>
                   <td><b>$total[total_berat] Gram</b></td>
                 </tr>
 
         </tbody>
       </table><br>
 
-      Silahkan mentransferkan uang dengan total <b>Rp ".rupiah($total['total']+$total['ongkir']+substr($kode_transaksi,-3))."</b> ke salah satu pilihan bank di bawah ini : <br> 
+      Silahkan mentransferkan uang dengan total <b>Rp " . rupiah($total['total'] + $total['ongkir'] + substr($kode_transaksi, -3)) . "</b> ke salah satu pilihan bank di bawah ini : <br> 
       <table class='table table-hover table-condensed table-bordered'>
         <thead>
           <tr bgcolor='#e3e3e3'>
@@ -86,16 +102,16 @@ if ($total['proses']=='0'){ $proses = '<i class="text-danger">Pending</i>'; }els
         </thead>
         <tbody>";
 
-            $noo = 1;
-            $rekening = $this->model_app->view('rb_rekening');
-            foreach ($rekening->result_array() as $row){
-            echo "<tr><td>$noo</td>
+$noo = 1;
+$rekening = $this->Model_app->view('rb_rekening');
+foreach ($rekening->result_array() as $row) {
+  echo "<tr><td>$noo</td>
                       <td>$row[nama_bank]</td>
                       <td>$row[no_rekening]</td>
                       <td>$row[pemilik_rekening]</td>
                   </tr>";
-              $noo++;
-            }
+  $noo++;
+}
 
-        echo "</tbody>
+echo "</tbody>
       </table>";
