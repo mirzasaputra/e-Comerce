1b6fc5e6d19cd4bec73bd7d74e4fadb7deb9b01b
dafidpr <dafidpr@gmail.com>
2020-09-06 22:43:56 +0700

    Menambahkan file script.php dan beberapa function didalamnya di mod_penjualan

---
+++ application/views/administrator/mod_penjualan/script.php
@@ -0,0 +1,244 @@
+<script type="text/javascript">
+    $('input:radio[name="metode"]').on('change', function() {
+        if ($(this).is(':checked') && $(this).val() == "Cash") {
+            $('.jatuh-tempo').hide();
+        } else if ($(this).is(':checked') && $(this).val() == "Kredit") {
+            $('.jatuh-tempo').show();
+        }
+    });
+
+    function customerCheck() {
+        var check = document.getElementById('customer-check');
+        if (check.checked == true) {
+            $('.customer-form').hide();
+        } else if (check.checked == false) {
+            $('.customer-form').show();
+        }
+    }
+
+    function selectProduk() {
+        const produk = $('#produk').val();
+        const check = document.getElementById('customer-check');
+        const customer = $('#customer').val();
+        $.ajax({
+            url: base_url + "administrator/detail_produk/" + produk,
+            type: "post",
+            success: function(data) {
+                var obj = JSON.parse(data);
+                $('#satuan').val(obj.satuan);
+                $('#stok').val(obj.stok);
+                if (obj.stok == 0) {
+                    Swal.fire({
+                        title: "Oops!",
+                        text: "Stok Produk Yang Anda Pilih Kosong!",
+                        icon: "error",
+                    });
+                } else {
+
+                    if (check.checked == true) {
+
+                        $('#harga').val(obj.harga_konsumen);
+
+                    } else if (check.checked == false) {
+                        $.ajax({
+                            url: base_url + "administrator/detail_customer/" + customer,
+                            type: "post",
+                            dataType: "json",
+                            success: function(result) {
+                                console.log(result);
+                                if (result.tipe == "Konsumen") {
+                                    $('#harga').val(obj.harga_konsumen);
+                                } else if (result.tipe == "Reseller") {
+                                    $('#harga').val(obj.harga_reseller);
+                                }
+                            }
+                        });
+
+                    }
+                }
+            }
+        });
+    }
+
+    function addItemJual() {
+        var data = $('.form-add-item').serialize();
+        const stok = $('#stok').val();
+        const qty = $('#qty').val();
+        const produk = $('#produk').val();
+        if (produk == '') {
+            Swal.fire({
+                title: "Oops!",
+                text: "Pilih Produk Terlebih Dahulu!",
+                icon: "error",
+            });
+        } else {
+
+            if (qty == '') {
+                Swal.fire({
+                    title: "Oops!",
+                    text: "Jumlah Produk Harus Diisi!",
+                    icon: "error",
+                });
+            } else {
+
+                if (qty > stok) {
+                    Swal.fire({
+                        title: "Oops!",
+                        text: "Jumlah Produk Melebihi Stok!",
+                        icon: "error",
+                    });
+                } else {
+
+                    $.ajax({
+                        url: base_url + "administrator/additemjual",
+                        type: "post",
+                        data: data,
+                        success: function(data) {
+                            var obj = JSON.parse(data);
+                            $('#grand-total').html(obj.subtotal);
+                            loadItemBeli();
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    function loadItemBeli() {
+        var html = '';
+        $.ajax({
+            url: base_url + "administrator/load_item_jual",
+            type: "post",
+            dataType: "json",
+            success: function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    html += "<tr><td>" + data[i].nama_produk + "</td>" +
+                        "<td>" + data[i].harga_jual + "</td>" +
+                        "<td>" + data[i].jumlah + "</td>" +
+                        "<td>" + data[i].diskon + "</td>" +
+                        "<td>" + data[i].subtotal + "</td>" +
+                        "<td><a class='btn btn-primary btn-xs' onclick='editItemJual(" + data[i].id_penjualan_detail + ")' title='Edit Data'><span class='glyphicon glyphicon-edit'></span></a> \n\ <a class='btn btn-danger btn-xs'  onclick='hapusItemJual(" + data[i].id_penjualan_detail + ")' title='Hapus Data'><span class='glyphicon glyphicon-remove'></span></a></td>"
+
+                }
+                $('#data-item-jual').html(html);
+            }
+        })
+    }
+
+    function hapusItemJual(e) {
+        Swal.fire({
+            title: "Are you sure ?",
+            text: "Deleted data can not be restored!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    url: base_url + "administrator/del_item_jual/" + e,
+                    type: "post",
+                    success: function(data) {
+                        loadItemBeli();
+                        var obj = JSON.parse(data);
+                        if (obj.subtotal != null) {
+                            $('#grand-total').text(obj.subtotal);
+                        } else {
+                            $('#grand-total').text(0);
+                        }
+                    }
+                })
+            }
+        })
+    }
+
+    function editItemJual(e) {
+        $.ajax({
+            url: base_url + "administrator/detail_item_jual/" + e,
+            type: "post",
+            success: function(data) {
+                var obj = JSON.parse(data);
+                $('#iddetiljual').val(obj.id_penjualan_detail);
+                $('#iddetilbarang').val(obj.id_produk);
+                $('#namadetilitem').val(obj.nama_produk);
+                $('#hargadetilitem').val(obj.harga_jual);
+                $('#detilqty').val(obj.jumlah);
+                $('#hideqty').val(obj.jumlah);
+                $('#detildiskonitem').val(obj.diskon);
+                $('#detiltotalitem').val(obj.subtotal);
+
+            }
+        });
+        $('#editDetilModal').modal('show');
+    }
+
+    function editDetilJual() {
+        var data = $('.form-edit-detil-jual').serialize();
+        $.ajax({
+            url: base_url + "administrator/edit_detail_penjualan",
+            type: "post",
+            data: data,
+            dataType: "json",
+            success: function(data) {
+                loadItemBeli();
+                if (data.subtotal != null) {
+                    $('#grand-total').text(data.subtotal);
+                } else {
+                    $('#grand-total').text(0);
+                }
+
+            }
+        });
+    }
+
+    $(function() {
+
+        var qty = $('#detilqty');
+        var diskon = $('#detildiskonitem');
+        var subtotal = $('#detiltotalitem');
+        var peritem = $('#hargadetilitem');
+        qty.keyup(function() {
+            var jum = document.getElementById('detilqty').value;
+            if (jum == null) {
+                subtotal.val(subtotal.val());
+            } else {
+                var total = peritem.val() * qty.val() - diskon.val();
+                subtotal.val(total);
+            }
+        });
+        diskon.keyup(function() {
+            var disk = document.getElementById('detildiskonitem').value;
+            if (disk == null) {
+                subtotal.val(subtotal.val());
+            } else {
+                var total = peritem.val() * qty.val() - diskon.val();
+                subtotal.val(total);
+            }
+        })
+    })
+
+    function checkoutPenjualan() {
+        // var cs = $('#customer').val();
+        // var user = $('#idoperator').val();
+        // $('#cus').val(cs);
+        // $('#kasir').val(user);
+        // $.ajax({
+        //     url: base_url + "penjualan/hargatotal",
+        //     type: "post",
+        //     success: function(data) {
+        //         var obj = JSON.parse(data);
+        //         var ppn = obj.subtotal * 10 / 100;
+        //         var hargaAkhir = ppn + Number(obj.subtotal);
+        //         $('#diskon1').val(obj.diskon);
+        //         $('#subtot').html(obj.subtotal);
+        //         $('#subtotal').val(obj.subtotal);
+        //         $('#grandtotal').val(obj.subtotal);
+        //         // $('#nominal_ppn').val(ppn);
+        //         $('#nominal').val(obj.subtotal);
+
+        //     }
+        // });
+        $('#pembayaranModal').modal('show');
+    }
+</script>
