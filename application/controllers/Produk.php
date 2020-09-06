0c167a45dcc6630b52e4227a80f8a177b310fe44
dafidpr <dafidpr@gmail.com>
2020-09-06 11:36:20 +0700

    Perbaikan kode pada table penjualan

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -165,7 +165,6 @@ class Produk extends CI_Controller
 					'ongkir' => $this->input->post('ongkir'),
 					'waktu_transaksi' => date('Y-m-d H:i:s'),
 					'proses' => '0',
-					'online_order'	=> 'Y'
 				);
 				$this->Model_app->insert('rb_penjualan', $data);
 				$idp = $this->db->insert_id();
