21915b5586eabfd54d61793d4ba13540d02fe911
dafidpr <dafidpr@gmail.com>
2020-09-08 18:51:15 +0700

    Memperbaiki tampilan form tracking dan form konfirmasi pembayaran, dan memperbaiki view testimoni

--- application/views/phpmu-one/pengunjung/view_tracking.php
+++ application/views/phpmu-one/pengunjung/view_tracking.php
@@ -1,15 +1,51 @@
-<p class='sidebar-title'><?php echo $title; ?></p>
-<?php 
-    $attributes = array('class'=>'form-horizontal','role'=>'form');
-    echo form_open_multipart('konfirmasi/tracking',$attributes); 
-    echo "<div class='alert alert-info'>Masukkan No Invoice atau No Transaksi Terlebih dahulu!</div>
-      <table class='table table-condensed'>
-        <tbody>
-          <tr><th scope='row' width='120px'>No Invoice</th>       <td><input type='text' name='a' class='form-control' style='width:100%' placeholder='TRX-0000000000' required>
-        </tbody>
-      </table>
+<?php
+// $attributes = array('class' => 'search-form', 'role' => 'form');
+// echo form_open_multipart('konfirmasi/tracking', $attributes);
+// echo "<div class='alert alert-info'>Masukkan No Invoice atau No Transaksi Terlebih dahulu!</div>
+//       <div class='search-top'>
+//         <input type='text' placeholder='Search here...' name='search'>
+//         <button value='search' type='submit'><i class='ti-search'></i></button>
+//     </div>
 
-      <div class='box-footer'>
-        <button type='submit' name='submit1' class='btn btn-info'>Cek Invoice</button>
-      </div>";
-    echo form_close();
+//       <div class='box-footer'>
+//         <button type='submit' name='submit1' class='btn btn-info'>Cek Invoice</button>
+//       </div>";
+// echo form_close();
+?>
+<!-- Start Checkout -->
+<section class="shop checkout section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-12">
+        <div class="checkout-form">
+          <h2>Tracking Order</h2>
+          <p>Cek keberadaan barang yang kamu pesan.</p>
+          <!-- Form -->
+          <form class="form" method="post" action="<?php echo base_url('konfirmasi/tracking') ?>">
+            <div class="row">
+              <div class="col-lg-9 col-md-9 col-12">
+                <div class="form-group">
+                  <label>Kode Transaksi<span>*</span></label>
+                  <input type="text" name="search" placeholder="Masukan Kode Transaksi Anda!" required="required" autocomplete="off">
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-12 col-12">
+                <div class="form-group" style="margin-top: 30px;">
+                  <div class="single-widget get-button">
+                    <div class="content">
+                      <div class="button">
+                        <button type="submit" name="submit1" class="btn">Cek Invoice</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+          <!--/ End Form -->
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--/ End Checkout -->
--- application/views/phpmu-one/view_testimoni.php
+++ application/views/phpmu-one/view_testimoni.php
@@ -1,33 +1,49 @@
-  <p class='sidebar-title'> Testimoni Konsumen</p>                
-                <table class='table table-condensed table-hover'>
-                  <?php 
-                  if (isset($_POST['submit'])){
-                      echo "<div class='alert alert-success'><center>Testimoni anda Succees Terkirim,.. <br> Testimoni akan muncul setelah disetujui oleh admin..</center></div>";
-                  }
+<section class="shop checkout section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-12">
+        <div class="checkout-form">
+          <h2>Testimoni Konsumen</h2>
+          <p>Beberapa konsumen telah belanja di toko kami dan mereka merasa puas akan produk kami.</p>
+          <!-- Form -->
 
-                  if ($this->session->id_konsumen != ''){
-                    $attributes = array('id' => 'formku','class'=>'form-horizontal','role'=>'form');
-                    echo form_open_multipart('testimoni',$attributes); 
-                      echo "<tr><td colspan='2'><textarea name='testimoni' style='height:70px' class='required form-control' placeholder='Tulis Testimoni Disini...' required></textarea> <br> 
-                                                <input name='submit' type='submit' style='margin-top:-15px' value='Kirimkan Testimoni' class='btn btn-primary btn-sm pull-right'></td></tr>";
-                    echo form_close();
-                  }
-                    $no = 0;
-                    foreach ($record->result_array() as $row){
-                      if (trim($row['foto'])==''){
-                        $foto = "users.gif";
-                      }else{
-                        $foto = $row['foto'];
-                      }
+          <!--/ End Form -->
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--/ End Checkout -->
+<!-- <table class='table table-condensed table-hover'> -->
+<?php
+// if (isset($_POST['submit'])) {
+//   echo "<div class='alert alert-success'><center>Testimoni anda Succees Terkirim,.. <br> Testimoni akan muncul setelah disetujui oleh admin..</center></div>";
+// }
 
-                    $tgl_posting = tgl_indo($row['tgl_posting']);
-                    echo "<tr><td><img width='70px' class='img-circle' src='".base_url()."asset/foto_user/$foto'></td>
-                              <td><div style='border-radius:0px; padding:6px; margin-bottom:3px' class='alert alert-success'><strong>$row[nama_lengkap]</strong></div> 
-                                $row[isi_testimoni]</td>
-                          </tr>";
-                      $no++;
-                    }
+// if ($this->session->id_konsumen != '') {
+//   $attributes = array('id' => 'formku', 'class' => 'form-horizontal', 'role' => 'form');
+//   echo form_open_multipart('testimoni', $attributes);
+//   echo "<tr><td colspan='2'><textarea name='testimoni' style='height:70px' class='required form-control' placeholder='Tulis Testimoni Disini...' required></textarea> <br> 
+//                                               <input name='submit' type='submit' style='margin-top:-15px' value='Kirimkan Testimoni' class='btn btn-primary btn-sm pull-right'></td></tr>";
+//   echo form_close();
+// }
+// $no = 0;
+// foreach ($record->result_array() as $row) {
+//   if (trim($row['foto']) == '') {
+//     $foto = "users.gif";
+//   } else {
+//     $foto = $row['foto'];
+//   }
 
-                echo "</table>";
-                echo "<div style='clear:both'></div>";
-                echo $this->pagination->create_links();
+//   $tgl_posting = tgl_indo($row['tgl_posting']);
+//   echo "<tr><td><img width='70px' class='img-circle' src='" . base_url() . "asset/foto_user/$foto'></td>
+//                             <td><div style='border-radius:0px; padding:6px; margin-bottom:3px' class='alert alert-success'><strong>$row[nama_lengkap]</strong></div> 
+//                               $row[isi_testimoni]</td>
+//                         </tr>";
+//   $no++;
+// }
+
+// echo "</table>";
+// echo "<div style='clear:both'></div>";
+// echo $this->pagination->create_links();
+?>
